2f58bbfd57c0bec92f228e8edc14fd8777db2424
ravi <ravindra@stavyah.com>
2025-03-27 13:00:41 +0530

    Env Changes done and working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  define: {
-    "process.env.API_BASE_URL": JSON.stringify(
-      process.env.NODE_ENV === "production"
-        ? "https://your-api.vercel.app"
-        : "http://localhost:3000"
-    )}
 })
