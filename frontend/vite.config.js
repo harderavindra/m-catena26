f77f798931e989bd394ca81d610206f8426d7244
ravi <ravindra@stavyah.com>
2025-03-28 19:16:01 +0530

    vite config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  server: {
+    host: '0.0.0.0',      // To allow external access
+    port: 3000,           // Custom port number
+    open: true,           // Automatically open in the browser
+    proxy: {
+      '/api': {
+        target: 'https://m-catena-b.vercel.app',  // Your backend URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),  // Optional rewrite rule
+      },
+    },
+  },
  
 })
